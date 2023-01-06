6244a09a96c4a59d5843b6890c40d2b25773736c
David Robles <drobles988@gmail.com>
2023-01-06 13:17:47 +0100

    creo stack view y le damos color a los botones

---
+++ iOS- Calculator/Controller/Home/HomeViewController.swift
@@ -0,0 +1,32 @@
+//
+//  HomeViewController.swift
+//  iOS- Calculator
+//
+//  Created by David Robles Lopez on 4/1/23.
+//
+
+import UIKit
+
+final class HomeViewController: UIViewController {
+    
+    // MARK: - Initialization
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Life cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+  
+
+}
